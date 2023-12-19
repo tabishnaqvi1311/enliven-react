9edc998e4e6e223f294e06796272ddfa2e7d5532
sai-ram-kaushik <sairamkaushik1@gmail.com>
2023-12-19 15:46:12 +0530

    updated with the landing page and login page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "./components/HomePage";
 import Navbar from "./components/Navbar";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
+import Support from "./components/Support";
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/support" element={<Support />} />
       </Routes>
     </div>
   );
---
+++ src/components/Support.jsx
@@ -0,0 +1,18 @@
+const Support = () => {
+  return (
+    <div className="w-full lg:h-[80vh] p-3">
+      <div className="container mx-auto w-full h-full">
+        <div className="flex flex-col items-center justify-center gap-3 w-full h-full">
+          <h2>Contact Us</h2>
+          <p>Any question or remarks? Just write us a message!</p>
+        </div>
+
+        <div className="flex flex-col items-center justify-center">
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Support;
